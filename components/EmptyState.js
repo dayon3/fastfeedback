4d3dcb63de34ef3888af9c4f07767069fdcfdef9
dayon3 <aliasdayone@gmail.com>
2021-01-23 22:53:41 +0300

    Created the dashboard shell

---
+++ components/EmptyState.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Flex, Heading, Text } from '@chakra-ui/react';
+import DashboardShell from './DashboardShell';
+import AddSiteModal from './AddSiteModal';
+
+const EmptyState = () => (
+  <DashboardShell>
+    <Flex
+      width="100%"
+      backgroundColor="white"
+      borderRadius="8px"
+      p={16}
+      justify="center"
+      align="center"
+      direction="column"
+    >
+      <Heading size="lg" mb={2}>
+        You haven't added any sites.
+      </Heading>
+      <Text mb={6}>Welcome 👋 Let's get started.</Text>
+      <AddSiteModal />
+    </Flex>
+  </DashboardShell>
+);
+
+export default EmptyState;
